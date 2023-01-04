b4a646dd03e8763f427cf5285080fed4c6ee4949
amypands <Amykpandya@gmail.com>
2023-01-03 20:32:31 -0500

    updated home

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,27 @@ export default function Home() {
         <div className="row gx-0 yorkheader">
           <div className="col-md-6 leftcolumn">
             <h1><strong>York Logic Ltd.</strong></h1>
-            <p>Expert support and solutions for your business's cloud computing needs</p>
-            <div>
-              <Button href="mailto:ketan@yorklogic.com" buttontype="primary">Contact Me</Button>
-              <Button href="/about" buttontype="secondary">About Me</Button>
+            <div className="row">
+              <div className="col-md-4 yorkdescription">
+                <p><strong>Simplify Technology</strong></p>
+                <p><strong>Be a Cloud Native in Google Cloud</strong></p>
+              </div>
+              <div className="col-md-8 yorkbullets">
+                <p>Custom software development</p>
+                <p>
+                  Digital Transformation</p>
+                <p>Google Cloud Architecture, Onboarding  and Transformation</p>
+                <p>DevOps and SRE</p>
+              </div>
             </div>
+            {/* <p>Expert support and solutions for your business's cloud computing needs</p> */}
+
           </div>
           <div className="col-md-6">
           </div>
         </div>
       </div>
-      <div className="row gx-0">
+      <div className="row gx-0 yorkcontainer">
         <div className="col-md-6">
         </div>
         <div className="col-md-6 yorkimage">
