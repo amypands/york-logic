219730925509c3b5aba33df3e82387e6e8291cd1
amypands <Amykpandya@gmail.com>
2023-01-03 20:08:18 -0500

    added navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<> <nav className="navbar navbar-expand-lg navbar-light container">
+    <a className="navbar-brand" href="#"><strong>York Logic Ltd.</strong></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Solutions </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">Clients</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav> <Component {...pageProps} /> </>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,36 @@ import { HeroBanner, Button } from '../components'
 
 export default function Home() {
   return (
-    <HeroBanner bannertitle="York Logic" bannertext="Expert support and solutions for your business's cloud computing needs" primarybutton="Contact Me" secondarybutton="About Me">
-      <Button href="mailto:ketan@yorklogic.com" buttontype="primary">Contact Me</Button>
-      <Button href="/about" buttontype="secondary">About Me</Button>
+    <>
+      <div className="container">
+        <div className="row gx-0 yorkheader">
+          <div className="col-md-6 leftcolumn">
+            <h1><strong>York Logic Ltd.</strong></h1>
+            <p>Expert support and solutions for your business's cloud computing needs</p>
+            <div>
+              <Button href="mailto:ketan@yorklogic.com" buttontype="primary">Contact Me</Button>
+              <Button href="/about" buttontype="secondary">About Me</Button>
+            </div>
+          </div>
+          <div className="col-md-6">
+          </div>
+        </div>
+      </div>
+      <div className="row gx-0">
+        <div className="col-md-6">
+        </div>
+        <div className="col-md-6 yorkimage">
+        </div>
+      </div>
+    </>
+
+
+    // <HeroBanner bannertitle="York Logic" bannertext="Expert support and solutions for your business's cloud computing needs" primarybutton="Contact Me" secondarybutton="About Me">
+    //   <Button href="mailto:ketan@yorklogic.com" buttontype="primary">Contact Me</Button>
+    //   <Button href="/about" buttontype="secondary">About Me</Button>
 
 
 
-    </HeroBanner>
+    // </HeroBanner>
   )
 }
