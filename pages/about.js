99478a68cbf421526dc4f401f400d7c4ef41c254
Jay Pandya <jaypandya2k5@gmail.com>
2023-01-31 21:11:04 -0500

    added meta tags

--- pages/about.js
+++ pages/about.js
@@ -1,73 +1,80 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { HeroBanner, Button, Card } from '../components'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { HeroBanner, Button, Card } from "../components";
 
 export default function About() {
-    const certList = ["Google Cloud Certified Professional Cloud Architect",
-        "Google Cloud Certified Professional Cloud Network Engineer",
-        "Google Cloud Certified Professional Cloud Security Engineer",
-        "CNCF Certified Kubernetes Administrator",
-        "CNCF Certified Kubernetes Developer",
-        "TOGAF"
-    ]
-    return (
-        <>
-            {/**
-             * To add the browser/tab title
-             */}
-            <Head>
-                <title>About</title>
-            </Head>
+  const certList = [
+    "Google Cloud Certified Professional Cloud Architect",
+    "Google Cloud Certified Professional Cloud Network Engineer",
+    "Google Cloud Certified Professional Cloud Security Engineer",
+    "CNCF Certified Kubernetes Administrator",
+    "CNCF Certified Kubernetes Developer",
+    "TOGAF",
+  ];
+  return (
+    <>
+      {/**
+       * To add the browser/tab title
+       */}
+      <Head>
+        <title>About</title>
+        <meta
+          name="keywords"
+          content="GCP consulting, Google Cloud Platform consulting, about us, company history, team"
+        />
+        <meta
+          name="description"
+          content="Learn about our company history and team of experts dedicated to providing top-notch GCP consulting services"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Ketan Pandya" />
+      </Head>
 
-            {/**
-             * Page Content
-             */}
-            <div className="about">
-                <div className="container my-4">
-                    <div className="row">
-                        <div className="col-xs-12 col-md-6">
-                            <h1><strong>About Us</strong></h1>
-                            <p>
-                                We are a Canadian company operating in the EST time zone. Our team has
-                                experience delivering results for start-ups to large enterprises.
-                                We are selective in taking on projects so that we can add value to
-                                our clients.
-                            </p>
+      {/**
+       * Page Content
+       */}
+      <div className="about">
+        <div className="container my-4">
+          <div className="row">
+            <div className="col-xs-12 col-md-6">
+              <h1>
+                <strong>About Us</strong>
+              </h1>
+              <p>
+                We are a Canadian company operating in the EST time zone. Our
+                team has experience delivering results for start-ups to large
+                enterprises. We are selective in taking on projects so that we
+                can add value to our clients.
+              </p>
 
-                            <p>
-                                We are start-up friendly and enterprise experienced. Our
-                                communication with clients is straight, simple and direct.
-                            </p>
+              <p>
+                We are start-up friendly and enterprise experienced. Our
+                communication with clients is straight, simple and direct.
+              </p>
 
-                            <p>
-                                We love Google Cloud and Kubernetes and have staff members with
-                                certifications to attest that.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="aboutmain">
-                    <div className="container  my-4">
-                        <h2 className="fw-bold mb-4">Certifications</h2>
-                        <div className="row g-4">
-                            {
-                                certList.map((certName) => {
-                                    return (
-
-                                        <div className="col-lg-3 col-md-3 col-sm-1">
-                                            <Card>
-                                                {certName}
-                                            </Card>
-                                        </div>
-                                    )
-                                }
-                                )
-                            }
-                        </div>
-                    </div>
-                </div></div>
-        </>
-    )
+              <p>
+                We love Google Cloud and Kubernetes and have staff members with
+                certifications to attest that.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="aboutmain">
+          <div className="container  my-4">
+            <h2 className="fw-bold mb-4">Certifications</h2>
+            <div className="row g-4">
+              {certList.map((certName) => {
+                return (
+                  <div className="col-lg-3 col-md-3 col-sm-1">
+                    <Card>{certName}</Card>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- pages/services.js
+++ pages/services.js
@@ -1,84 +1,97 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { HeroBanner, Button, Card } from '../components'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { HeroBanner, Button, Card } from "../components";
 
 export default function Services() {
-    const content = [
-        {
-            title: "Consulting",
-            description: "Short or long term adhoc or ongoing consultation in the area of needs - solutioning, Google Cloud, Cloud Native"
-        },
-        {
-            title: "Project collaborative outsourcing",
-            description: "We can provid solutioning, implementation, ongoing support and knowledge transfer"
-        }
-    ]
+  const content = [
+    {
+      title: "Consulting",
+      description:
+        "Short or long term adhoc or ongoing consultation in the area of needs - solutioning, Google Cloud, Cloud Native",
+    },
+    {
+      title: "Project collaborative outsourcing",
+      description:
+        "We can provid solutioning, implementation, ongoing support and knowledge transfer",
+    },
+  ];
 
-    return (
-        <>
-            {/**
-             * To add the browser/tab title
-             */}
-            <Head>
-                <title>About</title>
-            </Head>
+  return (
+    <>
+      {/**
+       * To add the browser/tab title
+       */}
+      <Head>
+        <title>About</title>
+        <meta
+          name="keywords"
+          content="GCP services, Google Cloud Platform services, cloud computing services, consulting services"
+        />
+        <meta
+          name="description"
+          content="Explore our wide range of GCP services, from consulting and implementation to ongoing support, designed to help your business succeed with Google Cloud Platform"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Ketan Pandya" />
+      </Head>
 
-            {/**
-             * Page Content
-             */}
-            <div className="solution-content">
-                <div className="container mt-4">
-                    <div className="row">
-                        <div className="col-md-6 pe-5 pt-5 solution-content-left">
-                            <h1 className="solution-title"><strong>Services</strong></h1>
-                            <div className="solution-line mb-3"></div>
-                            <h2>Expertly crafted GCP solutions tailored to your unique business needs</h2>
-                        </div>
-                        <div className="col-md-6 solution-content-right ps-5 pt-5">
-                            <h1 className="solution-title invisible"><strong>Services</strong></h1>
-                            <div className="solution-line mb-3"></div>
-                            <div>{
-                                content.map(
-                                    (contentItem) => {
-                                        return (
-                                            <div className="mb-5">
-                                                <h3><strong>{contentItem.title}</strong></h3>
-                                                <p>{contentItem.description}</p>
-
-                                            </div>
-                                        )
-                                    }
-                                )
-                            }
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      {/**
+       * Page Content
+       */}
+      <div className="solution-content">
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-md-6 pe-5 pt-5 solution-content-left">
+              <h1 className="solution-title">
+                <strong>Services</strong>
+              </h1>
+              <div className="solution-line mb-3"></div>
+              <h2>
+                Expertly crafted GCP solutions tailored to your unique business
+                needs
+              </h2>
             </div>
-            <div className="row solution-row">
-                <div className="col-md-6 solution-left yellow"></div>
-                <div className="col-md-6 solution-right">
-                    <div className="col-md-6 solution-content-right ps-5 pt-5">
-                        <div className="invisible">
-                            {
-                                content.map(
-                                    (contentItem) => {
-                                        return (
-                                            <div className="mb-4">
-                                                <h3><strong>{contentItem.title}</strong></h3>
-                                                <p>{contentItem.description}</p>
-                                            </div>
-                                        )
-                                    }
-                                )
-                            }
-                        </div>
+            <div className="col-md-6 solution-content-right ps-5 pt-5">
+              <h1 className="solution-title invisible">
+                <strong>Services</strong>
+              </h1>
+              <div className="solution-line mb-3"></div>
+              <div>
+                {content.map((contentItem) => {
+                  return (
+                    <div className="mb-5">
+                      <h3>
+                        <strong>{contentItem.title}</strong>
+                      </h3>
+                      <p>{contentItem.description}</p>
                     </div>
-                </div>
+                  );
+                })}
+              </div>
             </div>
-
-        </>
-    )
+          </div>
+        </div>
+      </div>
+      <div className="row solution-row">
+        <div className="col-md-6 solution-left yellow"></div>
+        <div className="col-md-6 solution-right">
+          <div className="col-md-6 solution-content-right ps-5 pt-5">
+            <div className="invisible">
+              {content.map((contentItem) => {
+                return (
+                  <div className="mb-4">
+                    <h3>
+                      <strong>{contentItem.title}</strong>
+                    </h3>
+                    <p>{contentItem.description}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
